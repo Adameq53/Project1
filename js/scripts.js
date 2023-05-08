231d6ae8739529d42ee22eea8808b3ecf4b90601
Adam <adnbhwbgf@gmail.com>
2023-05-08 12:26:59 +0200

    add .

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
     'use strict';
+
+    const btn = document.querySelector("header a")
+    const h1 = document.querySelector("headera h1")
+    const h1Text = h1.innerHTML;
+
+    btn.addEventListener('click' , () => {
+        h1.innerHTML = "Będzie pan zadowlony ;)"
+    });
