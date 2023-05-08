bfab92795dd75c3a46dfb4bb79a772dfca321fac
Adam <adnbhwbgf@gmail.com>
2023-05-08 12:42:49 +0200

    add .

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
     'use strict';
 
-    const btn = document.querySelector("header a")
-    const h1 = document.querySelector("headera h1")
+    const btn = document.querySelector("header a");
+    const h1 = document.querySelector("headera h1");
     const h1Text = h1.innerHTML;
-
+    let counetr = 0;
     btn.addEventListener('click' , () => {
-        h1.innerHTML = "Będzie pan zadowlony ;)"
+
+        counetr++;
+        // if(counetr % 2 === 0)
+        //     h1.innerHTML = h1Text;
+        // else
+        //     h1.innerHTML = "Będzie pan zadowlony ;)"
+
+        counetr % 2 === 0 ? h1.innerHTML = h1Text :  h1.innerHTML = ("Będzie pan zadowlony")
     });
